cff9ff5d81808e1c70fb0ff14dbba341817fa6da
Anujeet Swain <anujeet98@gmail.com>
2024-03-18 00:06:20 +0530

    cart button quantity change

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,13 +1,14 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import classes from './CartButton.module.css';
 import { cartSliceActions } from '../../Store/cartSlice';
 
 const CartButton = (props) => {
+  const Qty = useSelector(state=>state.cart.cart.length);
   const dispatch = useDispatch();
   return (
     <button className={classes.button} onClick={()=>{dispatch(cartSliceActions.toggleCart())}}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{Qty}</span>
     </button>
   );
 };
